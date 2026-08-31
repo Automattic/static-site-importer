fd6d40e7183f0a6f506c7e79cc226faaa4a0f37e
Chris Huber <chris.huber@automattic.com>
2026-08-31 00:11:56 -0400

    style: align lifecycle continuation state

--- includes/cli.php
+++ includes/cli.php
@@ -557,7 +557,7 @@ if ( ! function_exists( 'static_site_importer_cli_run_import_host' ) ) {
 			}
 			$input = static_site_importer_cli_apply_import_id( $input, $import_id );
 			if ( 'dependencies_prepared' === ( $result['continuation_reason'] ?? '' ) ) {
-				$prepared                             = is_array( $result['result'] ?? null ) ? $result['result'] : array();
+				$prepared                              = is_array( $result['result'] ?? null ) ? $result['result'] : array();
 				$input['runtime_lifecycle_phase']      = 'resume';
 				$input['runtime_lifecycle_request_id'] = (string) ( $prepared['fresh_runtime']['request_id'] ?? '' );
 				$input['runtime_lifecycle_checkpoint'] = (string) ( $prepared['fresh_runtime']['lifecycle_checkpoint_id'] ?? $prepared['runtime_lifecycle_checkpoint'] ?? '' );
