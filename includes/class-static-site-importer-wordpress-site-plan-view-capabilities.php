5d43264d6d10781738c5891eeb7f457707a77165
Chris Huber <chris.huber@automattic.com>
2026-09-09 23:48:06 -0400

    fix: defer compact view capability calls

--- includes/class-static-site-importer-wordpress-site-plan-view-capabilities.php
+++ includes/class-static-site-importer-wordpress-site-plan-view-capabilities.php
@@ -9,20 +9,33 @@ final class Static_Site_Importer_WordPress_Site_Plan_View_Capabilities {
 	private const VIEW_CLASS = '\\Automattic\\BlocksEngine\\PhpTransformer\\WordPressSitePlan\\WordPressSitePlanView';
 
 	public static function supports_compaction(): bool {
-		return class_exists( self::VIEW_CLASS ) && method_exists( self::VIEW_CLASS, 'compact' );
+		return class_exists( self::view_class() ) && method_exists( self::view_class(), 'compact' );
 	}
 
 	public static function supports_materialization(): bool {
-		return class_exists( self::VIEW_CLASS ) && method_exists( self::VIEW_CLASS, 'materialize' );
+		return class_exists( self::view_class() ) && method_exists( self::view_class(), 'materialize' );
 	}
 
 	/** @param array<string,mixed> $view @return array<string,mixed> */
 	public static function compact( array $view ): array {
-		return ( new ( self::VIEW_CLASS )() )->compact( $view );
+		$class    = self::view_class();
+		$callback = array( new $class(), 'compact' );
+		if ( ! is_callable( $callback ) ) {
+			throw new RuntimeException( 'The installed Blocks Engine php-transformer cannot compact WordPress site plan views.' );
+		}
+		return call_user_func( $callback, $view );
 	}
 
 	/** @param array<string,mixed> $view @return array<string,mixed> */
 	public static function materialize( array $view ): array {
-		return ( self::VIEW_CLASS )::materialize( $view );
+		$callback = array( self::view_class(), 'materialize' );
+		if ( ! is_callable( $callback ) ) {
+			throw new RuntimeException( 'The installed Blocks Engine php-transformer cannot materialize compact WordPress site plan views.' );
+		}
+		return call_user_func( $callback, $view );
+	}
+
+	private static function view_class(): string {
+		return self::VIEW_CLASS;
 	}
 }
